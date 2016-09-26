c8ac95d855cb96ce141f757786d321bcf2f39ec9
Michaelxiaoxi <w7655555@163.com>
2016-09-26 11:20:25 +0800

    Update zhihu.js

--- zhihu.js
+++ zhihu.js
@@ -2,6 +2,9 @@
 var http = require('http');
 var cheerio = require('cheerio');
 var url = 'http://www.zhihu.com/collection/69154204';
+var totalpage = 6;
+var page = 1;
+var i = 1;
 
 function fliterhtml(html){
     var $ = cheerio.load(html);
@@ -11,34 +14,46 @@ function fliterhtml(html){
         var star = $(this);
         var stitle = star.find('.zm-item-title').text();
         var anw =star.find('.zh-summary').text();
+        var zan = star.find('.zm-item-vote').text();
         var stitleData = {
             stitle : stitle,
-            ans :anw
+            ans :anw,
+            zan:zan
         }
-
         anws.push(stitleData);
     })
     return anws;
 }
 
 function printanwsinfo(anws){
-    console.log(anws.length);
-    var i=1;
+    console.log(">>>第"+page+"页")
     anws.forEach(function(item){
         var stitle = item.stitle;
-        console.log(i+' -- '+stitle +'\n');
+        var zan = item.zan;
+        console.log(i+'. '+stitle );
         i++
     })
+    page++;
+    if(page<=totalpage){
+        url = "http://www.zhihu.com/collection/69154204?page="+page;
+        again();
+    }else {
+        console.log("\n抓取完毕！")
+    }
 }
-http.get(url,function(res){
-    var html = '';
-    res.on('data',function(data){
-        html += data;
+
+function again() {
+    http.get(url, function (res) {
+        var html = '';
+        res.on('data', function (data) {
+            html += data;
+        })
+        res.on('end', function () {
+            var anws = fliterhtml(html);
+            printanwsinfo(anws);
+        })
+    }).on('error', function () {
+        console.log('error!!!')
     })
-    res.on('end',function(){
-       var anws =  fliterhtml(html);
-        printanwsinfo(anws);
-    })
-}).on('error',function(){
-    console.log('error!!!')
-})
+}
+again();
